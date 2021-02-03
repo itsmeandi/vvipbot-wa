e6f863f762cbb8c2b9179b8654378841a7377c16
itsmeandi <78503174+itsmeandi@users.noreply.github.com>
2021-02-04 03:34:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ const instagram = 'agus_alnsyh71- kaelMk_';
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN:Agus\n' // Sherlock Holmes
+            + 'ORG:VVIPINDO;\n' // Sherlock BOT
+            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //6282292119677
             + 'END:VCARD'
 const
 {
